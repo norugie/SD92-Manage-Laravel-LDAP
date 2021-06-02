eec30a08e61860c612b154cecf1f78c74defea77
Rugie Ann Barrameda <norugie015@gmail.com>
2021-06-02 12:47:08 -0700

    notification msg and routing for restricted page

--- resources/views/cms/layout/notification.blade.php
+++ resources/views/cms/layout/notification.blade.php
@@ -1,5 +1,5 @@
 @if(session()->exists('message'))
-    <div class="alert bg-green alert-dismissible" role="alert">
+    <div class="alert alert-{{ session('status') }} alert-dismissible" role="alert">
         <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
         {!! session('message') !!}
     </div>
