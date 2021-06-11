c6e396fb34b17b842e18d0d54c4d43cca278af56
Rugie Ann Barrameda <norugie015@gmail.com>
2021-06-11 12:56:09 -0700

    updating profile without full clearing groups

--- public/cms/js/emp-form.js
+++ public/cms/js/emp-form.js
@@ -1,12 +1,12 @@
-(function(){
+(function() {
     var config = "/cms/config.json";
-    
+
     $('#employee_department').change(function() {
         $('#employee_roles').empty();
         setRoles();
         setExtraRoles();
     });
-    
+
     function setRoles() {
         var optgroup = '<optgroup label="General Roles">';
         $.getJSON(config, function(data) {
@@ -18,26 +18,26 @@
             $.AdminBSB.select.refresh();
         });
     }
-    
+
     function setExtraRoles() {
         var local = $('#employee_department').val();
-    
+
         var optgrouproles = '<optgroup label="' + local + ' Specific Roles">';
         var optgroupdept = '<optgroup label="' + local + ' Sub-Departments">';
-    
+
         $.getJSON(config, function(data) {
             $.each(data['locations'][local]['local_roles'], function(key, value) {
                 optgrouproles += '<option value="' + key + '">' + value + '</option>';
             });
-    
+
             optgrouproles += "</optgroup>";
-    
+
             $.each(data['locations'][local]['departments'], function(key, value) {
                 optgroupdept += '<option value="dept-' + key + '">' + value + '</option>';
             });
-    
+
             optgroupdept += "</optgroup>";
-    
+
             $('#employee_roles').append(optgrouproles).append(optgroupdept);
             $.AdminBSB.select.refresh();
         });
