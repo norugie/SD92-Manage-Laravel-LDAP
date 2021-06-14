9e2772b596c83e182ac62b9a7f58e93add12c548
Rugie Ann Barrameda <norugie015@gmail.com>
2021-06-14 11:03:21 -0700

    setup for logs

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 use App\Models\Log;
+use Carbon\Carbon;
 
 class Controller extends BaseController
 {
@@ -18,5 +19,16 @@ class Controller extends BaseController
         $log->log_description = $description;
         $log->log_user = $user;
         $log->save();
+    }
+
+    public function requestLog ()
+    {
+        $log = new Log();
+
+        $startDate = Carbon::now()->subDays(30);
+        $endDate = Carbon::now();
+        return $log->whereBetween("created_at", [$startDate, $endDate])
+                ->orderBy('created_at', 'DESC')
+                ->get();
     }
 }
