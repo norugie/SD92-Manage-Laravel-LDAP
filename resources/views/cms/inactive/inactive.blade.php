03c68b6a36215ceb7b0933ec765e5cbc0c254106
Rugie Ann Barrameda <norugie015@gmail.com>
2021-06-22 12:59:03 -0700

    re-enabling accounts

---
+++ resources/views/cms/inactive/inactive.blade.php
@@ -0,0 +1,55 @@
+@extends ( 'cms.layout.layout' )
+
+@section ( 'content' )
+
+    <div class="row clearfix">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <div class="card">
+                    <div class="header">
+                        <div class="row clearfix">
+                            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-xs-sm-center">
+                                <h4>INACTIVE EMPLOYEE LIST</h4>      
+                            </div>
+                        </div>
+                    </div>
+                <div class="body">
+                    <div class="table-responsive">
+                        <table class="table table-bordered table-striped table-hover js-basic-example dataTable">
+                            <thead>
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Username</th>
+                                    <th>Email Address</th>
+                                    <th>Options</th>
+                                </tr>
+                            </thead>
+                            <tfoot>
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Username</th>
+                                    <th>Email Address</th>
+                                    <th>Options</th>
+                                </tr>
+                            </tfoot>
+                            <tbody>
+                                @foreach($employees as $employee)
+                                <tr>
+                                    <td>{{ $employee->getFirstAttribute('displayname') }}</td>
+                                    <td>{{ $employee->getFirstAttribute('samaccountname') }}</td>
+                                    <td>{{ $employee->getFirstAttribute('mail') }}</td>
+                                    <td>
+                                        <center>
+                                            <a href="/cms/inactive/{{ $employee->getFirstAttribute('samaccountname') }}/enable" type="button" class="btn bg-green waves-effect" style="display: inline-block;"><i class="material-icons">check</i><span>ENABLE EMPLOYEE</span></a>
+                                        </center>
+                                    </td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
