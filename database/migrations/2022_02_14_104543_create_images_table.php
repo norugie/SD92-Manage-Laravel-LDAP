ab7f5802f0944495d6ee94d6bdcfe77d93c6f60b
Rugie Ann Barrameda <norugie015@gmail.com>
2022-02-14 10:54:46 -0800

    image migration and model

---
+++ database/migrations/2022_02_14_104543_create_images_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->id();
+            $table->string('image_user');
+            $table->integer('image_user_id');
+            $table->mediumText('image_description');
+            $table->enum('image_status', ['Active', 'Inactive']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('images');
+    }
+};
