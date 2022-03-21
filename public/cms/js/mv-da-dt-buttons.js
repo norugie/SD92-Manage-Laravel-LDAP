2553d17f1b9f073096b66bca5597825e7e47a676
Rugie Ann Barrameda <norugie015@gmail.com>
2022-03-21 15:32:42 -0700

    fix js for modal list employee options

--- public/cms/js/mv-da-dt-buttons.js
+++ public/cms/js/mv-da-dt-buttons.js
@@ -1,28 +1,28 @@
 (function() {
-    $('.move-accounts').attr('disabled', true);
-    $('.disable-accounts').attr('disabled', true);
-
     $('#employee_table').on("change", ".employee-checkbox", function() {
+
         var employee = '#employee_checkbox_' + $(this).val();
         var employeeUsername = $(employee).val();
         var employeeFullname = $(employee).data('name');
         var employeeMoveUsernameList = $('#employee_multiple').val();
         var employeeDisableUsernameList = $('#employee_disable').val();
 
-        if (employeeMoveUsernameList.includes(employeeUsername + ',') && employeeDisableUsernameList.includes(employeeFullname + ',')) {
-            employeeMoveUsernameList = employeeMoveUsernameList.replace(employeeUsername + ',', '');
-            employeeDisableUsernameList = employeeDisableUsernameList.replace(employeeUsername + ',', '');
-            $('#employee-to-move_' + employeeUsername).remove();
-            $('#employee-to-disable_' + employeeUsername).remove();
-        } else {
+        if ($(employee).prop('checked')) {
             employeeMoveUsernameList = employeeMoveUsernameList + employeeUsername + ',';
             employeeDisableUsernameList = employeeDisableUsernameList + employeeUsername + ',';
             $('#employees-to-move').append('<li id="employee-to-move_' + employeeUsername + '">' + employeeFullname + '</li>');
             $('#employees-to-disable').append('<li id="employee-to-disable_' + employeeUsername + '">' + employeeFullname + '</li>');
+            console.log(employeeMoveUsernameList);
+        } else {
+            employeeMoveUsernameList = employeeMoveUsernameList.replace(employeeUsername + ',', '');
+            employeeDisableUsernameList = employeeDisableUsernameList.replace(employeeUsername + ',', '');
+            $('#employee-to-move_' + employeeUsername).remove();
+            $('#employee-to-disable_' + employeeUsername).remove();
+            console.log(employeeMoveUsernameList);
         }
 
-        $('#employee_multiple').attr('value', employeeMoveUsernameList);
-        $('#employee_disable').attr('value', employeeDisableUsernameList);
+        $('#employee_multiple').prop('value', employeeMoveUsernameList);
+        $('#employee_disable').prop('value', employeeDisableUsernameList);
 
         if ($('#employee_multiple').val().length === 0 || $('#employee_multiple').val().length === 0) {
             $('.move-accounts').attr('disabled', true).removeAttr('data-toggle').removeAttr('data-target');
