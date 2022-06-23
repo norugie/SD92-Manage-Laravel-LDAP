a59f0c1908523661460cee98f657d482881b4d81
Rugie Ann Barrameda <norugie015@gmail.com>
2022-06-23 15:06:38 -0700

    cleanup

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -10,14 +10,6 @@ class DashboardController extends Controller
 {
     public function index ()
     {
-        $message = 'An account for <b><a href="/cms/employees/rbarrameda/view" class="alert-link">Rugie Ann Barrameda</a></b> has been created successfully.';
-        $message =  $message . '<br><br>Please take note of the initial password for this user before closing this notice: <b>12345test</b>';
-        
-        Alert::html('New Account Created', $message, 'success');
-        // ->persistent(true)
-        // ->showCloseButton()
-        // ->showConfirmButton('CLOSE', '#607d8b');
-
         return view ( 'cms.dashboard.dashboard', [
             'logs' => $this->requestLog()
         ]);
