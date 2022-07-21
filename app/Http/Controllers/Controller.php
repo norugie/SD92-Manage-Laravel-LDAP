2a6a794c4ef780007a2764528e3afd47803343a4
Rugie Ann Barrameda <norugie015@gmail.com>
2022-07-21 13:30:33 -0700

    refactor notif for account availability check

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use LdapRecord\Models\Attributes\AccountControl;
 use App\Models\Log;
 use Carbon\Carbon;
 use Alert;
@@ -45,6 +46,28 @@ class Controller extends BaseController
                 ->orderBy('created_at', 'DESC')
                 ->get();
     }
+
+    /**
+     * Check user existence
+     * 
+     * @param mixed $user
+     * @return String $message
+     */
+
+     public function checkUser ($user)
+     {
+        $message = "";
+
+        // Check if $user exists
+        if($user === NULL) $message = 'The user you are looking for does not exist in our directory.';
+        else {
+            // Check if $user UAC is active
+            $uac = new AccountControl($user->getFirstAttribute('userAccountControl'));
+            if($uac->has(AccountControl::ACCOUNTDISABLE)) $message = 'The user you are looking for no longer has an active account in our directory.';
+        }
+
+        return $message;
+     }
 
     /**
      * Handle process for SweetAlert after-process alerts
